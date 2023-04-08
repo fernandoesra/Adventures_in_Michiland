09b223cdf2e329f855547ea41dcb2e206e5fb339
fernandoesra <fernando_sanvi3@hotmail.com>
2023-04-08 11:37:22 +0200

    Test push 2

--- desktop/src/com/mygdx/pruebafergdx/DesktopLauncher.java
+++ desktop/src/com/mygdx/pruebafergdx/DesktopLauncher.java
@@ -8,7 +8,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("Aventuras in Michiland");
+		config.setTitle("Adventures in Michiland");
 		config.setWindowedMode(960, 960);
 		new Lwjgl3Application(new pruebaMain(), config);
 	}
