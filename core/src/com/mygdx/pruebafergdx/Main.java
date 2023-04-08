2244af3ab1962f04b5e79312505b94d0a997aa06
MichaelValwin <macm241192@gmail.com>
2023-04-08 16:47:44 +0200

    Add Truetype to bitmap font Service

--- core/src/com/mygdx/pruebafergdx/Main.java
+++ core/src/com/mygdx/pruebafergdx/Main.java
@@ -6,7 +6,6 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
-import com.mygdx.pruebafergdx.ServicioTextura;
 
 /**
  * 
@@ -31,18 +30,17 @@ public class Main extends ApplicationAdapter {
 	public void create () {
 		spriteBatch = new SpriteBatch();
 		spriteSheet = new Texture("michi_tile_03.png");
-		// spriteTest = new Sprite(spriteSheet, 2, 2, 16, 16);
-		
-		orthoCamera = new OrthographicCamera(480,480);
-		// orthoCamera.translate(((heigthTotal/2)*16),((widthTotal/2)*16));
-		orthoCamera.position.set(((heigthTotal/2)*16), ((widthTotal/2)*16), 0);
-		orthoCamera.update();
 		
 		ServicioTextura.iniciarTexturas(spriteSheet);
+		ServicioTexto.inicializaFuente();
 		
 		tilemap = new Map(heigthTotal,widthTotal,spriteSheet);
 		
 		player = new Player((heigthTotal/2),(widthTotal/2));
+
+		orthoCamera = new OrthographicCamera(480,480);
+		orthoCamera.position.set(player.x*16, player.y*16, 0);
+		orthoCamera.update();
 	}
 
 	@Override
@@ -58,10 +56,12 @@ public class Main extends ApplicationAdapter {
 		
 		spriteBatch.setProjectionMatrix(orthoCamera.combined);
 		spriteBatch.begin();
+
 		tilemap.draw(spriteBatch);
-		// spriteTest.draw(spriteBatch);
-		
 		player.draw(spriteBatch);
+
+		// Test to draw some text
+		ServicioTexto.getBitmapFont().draw(spriteBatch, "Hello", player.x*16,player.y*16, 0, 0, false);
 		
 		spriteBatch.end();
 	}
---
+++ core/src/com/mygdx/pruebafergdx/ServicioTexto.java
@@ -0,0 +1,44 @@
+package com.mygdx.pruebafergdx;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+
+public class ServicioTexto {
+	
+	public static final String FONT_TRUE_TYPE_SOURCE = "fonts/PixeloidSans-JR6qo.ttf";
+
+	private static BitmapFont bitmapFont;
+
+	private ServicioTexto() {
+
+	}
+
+	public static void inicializaFuente() {
+		if(bitmapFont != null)bitmapFont.dispose();
+
+		// Load True type font
+		FreeTypeFontGenerator fontGenerator = new FreeTypeFontGenerator(Gdx.files.internal(FONT_TRUE_TYPE_SOURCE));
+
+		FreeTypeFontGenerator.FreeTypeFontParameter fontGenerationParams = new FreeTypeFontGenerator.FreeTypeFontParameter();
+		fontGenerationParams.size = 30;
+		fontGenerationParams.borderWidth = 1;
+		fontGenerationParams.color = Color.YELLOW;
+		fontGenerationParams.shadowOffsetX = 3;
+		fontGenerationParams.shadowOffsetY = 3;
+		fontGenerationParams.shadowColor = Color.BLACK;
+
+		bitmapFont = fontGenerator.generateFont(fontGenerationParams); // font size is 24 pixels
+		fontGenerator.dispose(); // Free resources
+	}
+
+	public static void dispose() {
+		bitmapFont.dispose();
+	}
+
+	public static BitmapFont getBitmapFont() {
+		return bitmapFont;
+	}
+
+}
