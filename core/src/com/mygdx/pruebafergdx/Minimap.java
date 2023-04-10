3b735d39b901c3477273e35040e7ba09a6c5237a
fernandoesra <fernando_sanvi3@hotmail.com>
2023-04-10 13:43:45 +0200

    Extend the minimap length and add legend

--- core/src/com/mygdx/pruebafergdx/Minimap.java
+++ core/src/com/mygdx/pruebafergdx/Minimap.java
@@ -13,9 +13,9 @@ public class Minimap {
 		int realHeigth = tilemap.length;
 		int realWidth = tilemap[0].length;
 		
-		char[][] minimap = new char[realHeigth/10][realWidth/10];
+		char[][] minimap = new char[realHeigth/10][(realWidth/10)*2];
 		int miniHeigth = realHeigth/10;
-		int miniWidth = realWidth/10;
+		int miniWidth = (realWidth/10)*2;
 		
 		for (int i = 0; i < miniHeigth; i++) {
 			for (int j = 0; j < miniWidth; j++) {
@@ -35,28 +35,74 @@ public class Minimap {
 		}
 		
 		// Always write the player at finish
-		minimap[playerY/10][playerX/10] = 'P';
+		minimap[playerY/10][(playerX/10)*2] = 'P';
 		
 		// Then draw the minimap
 		for (int i = 0; i < miniHeigth; i++) {
-			
 			if (i == 0) {
+				System.out.print("|");
 				for (int j = 0; j < miniWidth; j++) {
 					System.out.print("-");
 				}
-				System.out.println();
+				
+				System.out.println("|");
 			}
 			
+			System.out.print("|");
 			for (int j = 0; j < miniWidth; j++) {
-				System.out.print(minimap[i][j]);
+				System.out.print(minimap[i][j]);				
+				
 			}
+			System.out.print("|");
+			
+			if (i == 3) {
+				System.out.print("           N");
+			}
+			if (i == 4) {
+				System.out.print("          S E");
+			}
+			if (i == 5) {
+				System.out.print("           W");
+			}
+			if (i == 7) {
+				System.out.print("   --------------------");
+			}
+			if (i == 8) {
+				System.out.print("   | Michiland map    |");
+			}
+			if (i == 9) {
+				System.out.print("   |                  |");
+			}
+			if (i == 10) {
+				System.out.print("   | Legend           |");
+			}
+			if (i == 11) {
+				System.out.print("   |                  |");
+			}
+			if (i == 12) {
+				System.out.print("   | X: Cave          |");
+			}
+			if (i == 13) {
+				System.out.print("   | J: Objetive      |");
+			}
+			if (i == 14) {
+				System.out.print("   | P: You           |");
+			}
+			if (i == 15) {
+				System.out.print("   |                  |");
+			}
+			if (i == 16) {
+				System.out.print("   --------------------");
+			}
+			
 			System.out.println();
 			
 			if (i == miniHeigth-1) {
+				System.out.print("|");
 				for (int j = 0; j < miniWidth; j++) {
 					System.out.print("-");
 				}
-				System.out.println();
+				System.out.println("|");
 			}
 		}
 		
